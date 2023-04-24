63b14be8cddb7c74423d529b5ea316e527a753dd
dhkatz <dhkatz.16@gmail.com>
2023-04-23 21:39:45 -0700

    Add support for SFTP using WinSCP

--- AcManager.Tools/Helpers/Ftp/BasicFtpClient.cs
+++ AcManager.Tools/Helpers/Ftp/BasicFtpClient.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using AcTools.Utils.Helpers;
 using FirstFloor.ModernUI.Helpers;
-using FirstFloor.ModernUI.Serialization;
 using JetBrains.Annotations;
 
-namespace AcManager.Tools.Helpers {
+namespace AcManager.Tools.Helpers.Ftp {
     // Based on https://www.codeproject.com/Tips/443588/Simple-Csharp-FTP-Class, but changed quite a bit
-    public class FtpClient {
+    public class BasicFtpClient : FtpClient
+    {
         [NotNull]
         private readonly string _hostName;
 
@@ -29,7 +26,7 @@ namespace AcManager.Tools.Helpers {
         public bool KeepAlive { get; set; } = true;
         public TimeSpan Timeout { get; set; } = TimeSpan.MaxValue;
 
-        public FtpClient([NotNull] string hostName, [CanBeNull] string userNameName, [CanBeNull] string password) {
+        public BasicFtpClient([NotNull] string hostName, [CanBeNull] string userNameName, [CanBeNull] string password) {
             _hostName = hostName;
             _userName = userNameName;
             _password = password;
@@ -47,7 +44,7 @@ namespace AcManager.Tools.Helpers {
             return killer;
         }
 
-        public async Task DownloadAsync([NotNull] string remoteFile, [NotNull] string localFile, CancellationToken cancellation = default) {
+        public override async Task DownloadAsync([NotNull] string remoteFile, [NotNull] string localFile, CancellationToken cancellation = default) {
             using (var killer = Request(remoteFile, WebRequestMethods.Ftp.DownloadFile, cancellation))
             using (var response = (FtpWebResponse)await killer.Victim.GetResponseAsync().ConfigureAwait(false))
             using (var stream = response.GetResponseStream()) {
@@ -59,7 +56,7 @@ namespace AcManager.Tools.Helpers {
             }
         }
 
-        public async Task UploadAsync([NotNull] string remoteFile, [NotNull] string localFile, CancellationToken cancellation = default) {
+        public override async Task UploadAsync(string remoteFile, string localFile, CancellationToken cancellation = default) {
             using (var killer = Request(remoteFile, WebRequestMethods.Ftp.UploadFile, cancellation))
             using (var local = new FileStream(localFile, FileMode.Open))
             using (var stream = await killer.Victim.GetRequestStreamAsync()) {
@@ -69,32 +66,32 @@ namespace AcManager.Tools.Helpers {
             }
         }
 
-        public async Task DeleteAsync([NotNull] string remoteFile, CancellationToken cancellation = default) {
+        public override async Task DeleteAsync(string remoteFile, CancellationToken cancellation = default) {
             using (var killer = Request(remoteFile, WebRequestMethods.Ftp.DeleteFile, cancellation)) {
                 await killer.Victim.GetResponseAsync().ConfigureAwait(false);
             }
         }
 
-        public async Task RenameAsync([NotNull] string remoteFile, [NotNull] string newFileName, CancellationToken cancellation = default) {
+        public override async Task RenameAsync(string remoteFile, string newFileName, CancellationToken cancellation = default) {
             using (var killer = Request(remoteFile, WebRequestMethods.Ftp.Rename, cancellation)) {
                 killer.Victim.RenameTo = newFileName;
                 await killer.Victim.GetResponseAsync().ConfigureAwait(false);
             }
         }
 
-        public async Task DeleteDirectoryAsync([NotNull] string newDirectory, CancellationToken cancellation = default) {
+        public override async Task DeleteDirectoryAsync( string newDirectory, CancellationToken cancellation = default) {
             using (var killer = Request(newDirectory, WebRequestMethods.Ftp.RemoveDirectory, cancellation)) {
                 await killer.Victim.GetResponseAsync().ConfigureAwait(false);
             }
         }
 
-        public async Task CreateDirectoryAsync([NotNull] string newDirectory, CancellationToken cancellation = default) {
+        public override async Task CreateDirectoryAsync( string newDirectory, CancellationToken cancellation = default) {
             using (var killer = Request(newDirectory, WebRequestMethods.Ftp.MakeDirectory, cancellation)) {
                 await killer.Victim.GetResponseAsync().ConfigureAwait(false);
             }
         }
 
-        public async Task CleanDirectoryAsync([NotNull] string directory, CancellationToken cancellation = default){
+        public override async Task CleanDirectoryAsync( string directory, CancellationToken cancellation = default){
             var files = await DirectoryListDetailedAsync(directory, cancellation).ConfigureAwait(false);
             foreach (var file in files){
                 var filePath = $@"{directory}/{file.FileName}";
@@ -107,7 +104,7 @@ namespace AcManager.Tools.Helpers {
             }
         }
 
-        public async Task<string> GetFileCreatedDateTimeAsync([NotNull] string remoteFile, CancellationToken cancellation = default) {
+        public override async Task<string> GetFileCreatedDateTimeAsync( string remoteFile, CancellationToken cancellation = default) {
             using (var killer = Request(remoteFile, WebRequestMethods.Ftp.GetDateTimestamp, cancellation))
             using (var response = (FtpWebResponse)await killer.Victim.GetResponseAsync().ConfigureAwait(false))
             using (var stream = response.GetResponseStream()) {
@@ -117,7 +114,7 @@ namespace AcManager.Tools.Helpers {
             }
         }
 
-        public async Task<string> GetFileSizeAsync([NotNull] string remoteFile, CancellationToken cancellation = default) {
+        public override async Task<string> GetFileSizeAsync( string remoteFile, CancellationToken cancellation = default) {
             using (var killer = Request(remoteFile, WebRequestMethods.Ftp.GetFileSize, cancellation))
             using (var response = (FtpWebResponse)await killer.Victim.GetResponseAsync().ConfigureAwait(false))
             using (var stream = response.GetResponseStream()) {
@@ -127,7 +124,7 @@ namespace AcManager.Tools.Helpers {
             }
         }
 
-        public async Task<string[]> DirectoryListSimpleAsync([NotNull] string remoteFile, CancellationToken cancellation = default) {
+        public override async Task<string[]> DirectoryListSimpleAsync( string remoteFile, CancellationToken cancellation = default) {
             using (var killer = Request(remoteFile, WebRequestMethods.Ftp.ListDirectory, cancellation))
             using (var response = (FtpWebResponse)await killer.Victim.GetResponseAsync().ConfigureAwait(false))
             using (var stream = response.GetResponseStream()) {
@@ -137,43 +134,7 @@ namespace AcManager.Tools.Helpers {
             }
         }
 
-		public class DetailedInformation {
-			public bool IsDirectory { get; }
-			public long Size { get; }
-			public DateTime LastWriteTime { get; }
-			public string FileName { get; }
-
-			private DetailedInformation(bool isDirectory, long size, DateTime lastWriteTime, string fileName){
-				IsDirectory = isDirectory;
-				Size = size;
-				LastWriteTime = lastWriteTime;
-				FileName = fileName;
-			}
-
-			public static DetailedInformation[] Create(string data) {
-			    return data.TrimEnd().Split('\n').Select(x => new {
-			        Type = x.Length < 1 ? '\0' : x[0],
-			        Match = Regex.Match(x, @"\s(\d+) ((?:Feb|Ma[ry]|A(?:pr|ug)|J(?:an|u[ln])|Sep|Oct|Nov|Dec) (?: \d|\d[\d ])) ( \d{4}|\d\d:\d\d) (.+?)(?:->.+)?$",
-			                RegexOptions.IgnoreCase)
-			    }).Select(x => {
-			        var fileName = x.Match.Groups[4].Value.Trim();
-			        return new DetailedInformation(IsDirectory(x.Type, fileName), x.Match.Groups[1].As<long>(), GetDate(x.Match.Groups), fileName);
-			    }).ToArray();
-
-				bool IsDirectory(char type, string fileName){
-					return type == 'd' || type == 'l' && fileName.LastIndexOf('.') < fileName.Length - 4;
-				}
-
-				DateTime GetDate(GroupCollection p) {
-				    var year = p[3].Length == 4 ? p[3].As<int>() : DateTime.Now.Year;
-				    var monthAndDay = p[2];
-				    var time = p[3].Length == 4 ? @"00:00" : p[3].Value;
-				    return DateTime.Parse($@"{year} {monthAndDay} {time}", CultureInfo.InvariantCulture);
-				}
-			}
-		}
-
-        public async Task<DetailedInformation[]> DirectoryListDetailedAsync([NotNull] string remoteFile, CancellationToken cancellation = default) {
+        internal override async Task<DetailedInformation[]> DirectoryListDetailedAsync( string remoteFile, CancellationToken cancellation = default) {
             using (var killer = Request(remoteFile, WebRequestMethods.Ftp.ListDirectoryDetails, cancellation))
             using (var response = (FtpWebResponse)await killer.Victim.GetResponseAsync().ConfigureAwait(false))
             using (var stream = response.GetResponseStream()) {
---
+++ AcManager.Tools/Helpers/Ftp/WinScpClient.cs
@@ -0,0 +1,244 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using AcTools.Utils.Helpers;
+using JetBrains.Annotations;
+using WinSCP;
+
+namespace AcManager.Tools.Helpers.Ftp
+{
+	public class WinScpClient : FtpClient
+	{
+		private readonly SessionOptions _sessionOptions;
+		
+		public WinScpClient([NotNull] string hostname, [CanBeNull] string username, [CanBeNull] string password)
+		{
+			_sessionOptions = new SessionOptions();
+
+			_sessionOptions.ParseUrl(hostname);
+			
+			if (!string.IsNullOrEmpty(username))
+			{
+				_sessionOptions.UserName = username;
+			}
+			
+			if (!string.IsNullOrEmpty(password))
+			{
+				_sessionOptions.Password = password;
+			}
+
+			_sessionOptions.SshHostKeyPolicy = SshHostKeyPolicy.AcceptNew;
+		}
+
+		public override async Task DownloadAsync(string remoteFile, string localFile, CancellationToken cancellation = default)
+		{
+			using (Session session = new Session())
+			{
+				await Task.Run(() =>
+				{
+					session.DisableVersionCheck = true;
+					session.Open(_sessionOptions);
+					var stream = session.GetFile(remoteFile);
+					using (FileStream file = new FileStream(localFile, FileMode.Create))
+					{
+						stream.CopyTo(file);
+					}
+					
+				}, cancellation);
+			}
+		}
+
+		public override async Task UploadAsync(string remoteFile, string localFile, CancellationToken cancellationToken = default)
+		{
+			using (FileStream file = new FileStream(localFile, FileMode.Open))
+			using (Session session = new Session())
+			{
+				await Task.Run(() =>
+				{
+					session.DisableVersionCheck = true;
+					session.Open(_sessionOptions);
+					session.PutFile(file, remoteFile);
+				}, cancellationToken);
+			}
+		}
+		
+		public override async Task DeleteAsync(string remoteFile, CancellationToken cancellationToken = default)
+		{
+			using (Session session = new Session())
+			{
+				await Task.Run(() =>
+				{
+					session.DisableVersionCheck = true;
+					session.Open(_sessionOptions);
+					session.RemoveFile(remoteFile);
+				}, cancellationToken);
+			}
+		}
+
+		public override async Task RenameAsync(string remoteFile, string newFileName, CancellationToken cancellation = default)
+		{
+			using (Session session = new Session())
+			{
+				await Task.Run(() =>
+				{
+					session.DisableVersionCheck = true;
+					session.Open(_sessionOptions);
+					session.MoveFile(remoteFile, newFileName);
+				}, cancellation);
+			}
+		}
+
+		public override async Task DeleteDirectoryAsync(string remoteDirectory, CancellationToken cancellationToken = default)
+		{
+			using (Session session = new Session())
+			{
+				await Task.Run(() =>
+				{
+					session.DisableVersionCheck = true;
+					session.Open(_sessionOptions);
+					session.RemoveFiles(remoteDirectory);
+				}, cancellationToken);
+			}
+		}
+		
+		public override async Task CreateDirectoryAsync(string remoteDirectory, CancellationToken cancellationToken = default)
+		{
+			using (Session session = new Session())
+			{
+				await Task.Run(() =>
+				{
+					session.DisableVersionCheck = true;
+					session.Open(_sessionOptions);
+					session.CreateDirectory(remoteDirectory);
+				}, cancellationToken);
+			}
+		}
+
+		public override async Task CleanDirectoryAsync(string directory, CancellationToken cancellation = default)
+		{
+			var files = await DirectoryListDetailedAsync(directory, cancellation).ConfigureAwait(false);
+			foreach (DetailedInformation file in files)
+			{
+				var filePath = $@"{directory}/{file.FileName}";
+				if (file.IsDirectory)
+				{
+					await CleanDirectoryAsync(filePath, cancellation).ConfigureAwait(false);
+					await DeleteDirectoryAsync(filePath, cancellation).ConfigureAwait(false);
+				}
+				else
+				{
+					await DeleteAsync(filePath, cancellation).ConfigureAwait(false);
+				}
+			}
+		}
+
+		public override async Task<string> GetFileCreatedDateTimeAsync(string remoteFile, CancellationToken cancellation = default)
+		{
+			using (Session session = new Session())
+			{
+				return await Task.Run(() =>
+				{
+					session.DisableVersionCheck = true;
+					session.Open(_sessionOptions);
+					return session.GetFileInfo(remoteFile).LastWriteTime.ToString(CultureInfo.CurrentCulture);
+				}, cancellation);
+			}
+		}
+
+		public override async Task<string> GetFileSizeAsync(string remoteFile, CancellationToken cancellation = default)
+		{
+			using (Session session = new Session())
+			{
+				return await Task.Run(() =>
+				{
+					session.DisableVersionCheck = true;
+					session.Open(_sessionOptions);
+					return session.GetFileInfo(remoteFile).Length.ToString();
+				}, cancellation);
+			}
+		}
+
+		public override async Task<string[]> DirectoryListSimpleAsync(string remoteFile, CancellationToken cancellation = default)
+		{
+			// Return list of file names in the directory
+			using (Session session = new Session())
+			{
+				return await Task.Run(() =>
+				{
+					session.DisableVersionCheck = true;
+					session.Open(_sessionOptions);
+
+					if (!session.FileExists(remoteFile))
+					{
+						return new string[0];
+					}
+
+					var file = session.GetFileInfo(remoteFile);
+
+					if (file.IsDirectory)
+					{
+						var directory = session.ListDirectory(remoteFile);
+						var fileNames = new string[directory.Files.Count];
+
+						foreach (RemoteFileInfo fileInfo in directory.Files)
+						{
+							fileNames[directory.Files.IndexOf(fileInfo)] = fileInfo.Name;
+						}
+
+						return fileNames;
+					}
+
+					return new[] { file.Name };
+				}, cancellation);
+			}
+		}
+
+		internal override async Task<DetailedInformation[]> DirectoryListDetailedAsync(string remoteFile, CancellationToken cancellationToken = default)
+		{
+			return await Task.Run(() =>
+			{
+				using (Session session = new Session())
+				{
+					session.DisableVersionCheck = true;
+					session.Open(_sessionOptions);
+
+					if (!session.FileExists(remoteFile))
+					{
+						return new DetailedInformation[0];
+					}
+
+					var file = session.GetFileInfo(remoteFile);
+
+					if (file.IsDirectory)
+					{
+						var directory = session.ListDirectory(remoteFile);
+						var detailedInformation = new DetailedInformation[directory.Files.Count];
+
+						foreach (RemoteFileInfo fileInfo in directory.Files)
+						{
+							detailedInformation[directory.Files.IndexOf(fileInfo)] = new DetailedInformation(
+								fileInfo.IsDirectory,
+								fileInfo.Length,
+								fileInfo.LastWriteTime,
+								fileInfo.Name
+							);
+						}
+
+						return detailedInformation;
+					}
+
+					return new[]
+					{
+						new DetailedInformation(
+							file.IsDirectory,
+							file.Length,
+							file.LastWriteTime,
+							file.Name
+						)
+					};
+				}
+			}, cancellationToken);
+		}
+	}
+}
